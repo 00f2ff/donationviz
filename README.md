dd1ac0326ae237cc04ac90aaf5623080c325ba88
Duncan McIsaac <dbmcisaac1@gmail.com>
2015-11-01 14:15:33 -0500

    updated instructions

---
+++ README.md
@@ -0,0 +1,3 @@
+OpenSecrets instructions: 
+
+Copy the contents of keys_blank.py into a new file named keys.py. Add your API key to that class and save. 
--- backend/keys_blank.py
+++ backend/keys_blank.py
@@ -1,6 +1,3 @@
-"""
-Rename this file 'keys.py' and add in your own OpenSecrets API keys
-"""
 class Keys(object):
 	def __init__(self):
 		self.apikey = ""
