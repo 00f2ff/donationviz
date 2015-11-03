4326eaac04d8b12a93c007dee0d112b7171c0243
Duncan McIsaac <dbmcisaac1@gmail.com>
2015-11-03 17:48:44 -0500

    still working on org stuff

--- backend/senatorContributions.py
+++ backend/senatorContributions.py
@@ -4,7 +4,7 @@ import urllib2
 import re
 import string
 import csv
-
+import ast
 
 # Find Senator data (there are 102 for some reason)
 def findSenators():
@@ -51,7 +51,7 @@ def findSenatorContributions(cid):
 		con = urllib2.urlopen(req)
 		# convert into dictionary
 		response = json.loads(con.read())
-	except UnicodeDecodeError:
+	except UnicodeDecodeError: # ***************** FIX THIS METHOD TO CONVERT UNICODE INTO PROPER DICT; IT BREAKS THINGS LATER
 		opener = urllib2.build_opener()
 		opener.addheaders = [('User-agent', headers)]
 		con = opener.open(url)
@@ -85,6 +85,64 @@ def writeSenContribsToIDFiles():
 
 	print "~~~ fin ~~~"
 
+# Find Senator data (there are 102 for some reason) and return as dictionary
+def findSenatorsAsDict():
+	senators = {}
+	with open('data/114th.csv', 'rU') as csvfile:
+		reader = csv.reader(csvfile)
+		for row in reader:
+			if row[4][0] == "S":
+				candidate = {
+				'cid': row[0],
+				'first_name': row[1].split(',')[1],
+				'last_name': row[1].split(',')[0],
+				'party': row[2],
+				'state': row[3][:2],
+				'class': row[3][3], # class is because the elections are staggered
+				'fec_id': row[4]
+				}
+				senators[row[0]] = candidate
+	return senators
+
+
+def writeOrganizationDataToFile():
+	senators = findSenatorsAsDict()
+	organizations = {}
+	with open("data/senContribAll.json", "r") as f:
+		data = json.loads(f.read())["senContrib"]
+		for record in data:
+			cid = record.keys()[0]
+			for donation in record[cid]:
+				# check if unicode
+				print type(donation)
+				if not isinstance(donation, dict): # (this may create problems in JSON decoding on JS end)
+					# turn into dict (this is so bad)
+					donation = dict(donation)
+					# donation = ast.literal_eval(str(donation))
+				# base case
+				if donation["organization"] not in organizations.keys():
+					organizations[donation["organization"]] = {"donations":[], "states":{}}
+				# add donations to this senator
+				senatorWithDonation = senators[cid].copy()
+				senatorWithDonation["pac"] = donation["pac"]
+				senatorWithDonation["indivs"] = donation["indivs"]
+				senatorWithDonation["total"] = donation["totals"]
+				organizations[donation["organization"]]["donations"].append(senatorWithDonation)
+				# add donations to state
+				if senatorWithDonation["state"] not in organizations[donation["organization"]]["states"]:
+					organizations[donation["organization"]]["states"][senatorWithDonation["state"]] = {"pac":0,"indivs":0,"total":0}
+				organizations[donation["organization"]]["states"][senatorWithDonation["state"]]["pac"] += int(donation["pac"])
+				organizations[donation["organization"]]["states"][senatorWithDonation["state"]]["indivs"] += int(donation["indivs"])
+				organizations[donation["organization"]]["states"][senatorWithDonation["state"]]["total"] += int(donation["totals"])
+		with open("data/organizations.json", "w") as orgf:
+			orgs = {"organizations": organizations}
+			orgs = str(json.dumps(orgs, indent=2))
+			orgf.write(orgs)
+
+					
+
+
+writeOrganizationDataToFile()
 
 # writeStateSenatorsToFile()
 # writeSenContribsToSingleFile()
