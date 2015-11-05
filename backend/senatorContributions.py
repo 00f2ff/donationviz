b7e7f9f6393ca330d249ebd4b58f339e574f95a6
Duncan McIsaac <dbmcisaac1@gmail.com>
2015-11-05 18:40:14 -0500

    got organizations.json code working. Not sure about best way to format yet, so it's in a monolithic json file. Also, data doesn't completely match that of tables on OpenSecrets, but their site may not update this information as regularly as they do the files the data came from

--- backend/senatorContributions.py
+++ backend/senatorContributions.py
@@ -112,28 +112,30 @@ def writeOrganizationDataToFile():
 		data = json.loads(f.read())["senContrib"]
 		for record in data:
 			cid = record.keys()[0]
-			for donation in record[cid]:
-				# check if unicode
-				print type(donation)
-				if not isinstance(donation, dict): # (this may create problems in JSON decoding on JS end)
-					# turn into dict (this is so bad)
-					donation = dict(donation)
-					# donation = ast.literal_eval(str(donation))
-				# base case
-				if donation["organization"] not in organizations.keys():
-					organizations[donation["organization"]] = {"donations":[], "states":{}}
-				# add donations to this senator
-				senatorWithDonation = senators[cid].copy()
-				senatorWithDonation["pac"] = donation["pac"]
-				senatorWithDonation["indivs"] = donation["indivs"]
-				senatorWithDonation["total"] = donation["totals"]
-				organizations[donation["organization"]]["donations"].append(senatorWithDonation)
-				# add donations to state
-				if senatorWithDonation["state"] not in organizations[donation["organization"]]["states"]:
-					organizations[donation["organization"]]["states"][senatorWithDonation["state"]] = {"pac":0,"indivs":0,"total":0}
-				organizations[donation["organization"]]["states"][senatorWithDonation["state"]]["pac"] += int(donation["pac"])
-				organizations[donation["organization"]]["states"][senatorWithDonation["state"]]["indivs"] += int(donation["indivs"])
-				organizations[donation["organization"]]["states"][senatorWithDonation["state"]]["total"] += int(donation["totals"])
+			# for some reason this analyzes record.keys()[1] as well
+			if cid != "response":
+				for donation in record[cid]:
+					# check if unicode
+
+					# if not isinstance(donation, dict): # (this may create problems in JSON decoding on JS end)
+					# 	# turn into dict (this is so bad)
+					# 	donation = dict(donation)
+						# donation = ast.literal_eval(str(donation))
+					# base case
+					if donation["organization"] not in organizations.keys():
+						organizations[donation["organization"]] = {"donations":[], "states":{}}
+					# add donations to this senator
+					senatorWithDonation = senators[cid].copy()
+					senatorWithDonation["pac"] = donation["pac"]
+					senatorWithDonation["indivs"] = donation["indivs"]
+					senatorWithDonation["total"] = donation["totals"]
+					organizations[donation["organization"]]["donations"].append(senatorWithDonation)
+					# add donations to state
+					if senatorWithDonation["state"] not in organizations[donation["organization"]]["states"]:
+						organizations[donation["organization"]]["states"][senatorWithDonation["state"]] = {"pac":0,"indivs":0,"total":0}
+					organizations[donation["organization"]]["states"][senatorWithDonation["state"]]["pac"] += int(donation["pac"])
+					organizations[donation["organization"]]["states"][senatorWithDonation["state"]]["indivs"] += int(donation["indivs"])
+					organizations[donation["organization"]]["states"][senatorWithDonation["state"]]["total"] += int(donation["totals"])
 		with open("data/organizations.json", "w") as orgf:
 			orgs = {"organizations": organizations}
 			orgs = str(json.dumps(orgs, indent=2))
@@ -142,7 +144,7 @@ def writeOrganizationDataToFile():
 					
 
 
-# writeOrganizationDataToFile()
+writeOrganizationDataToFile()
 
 # writeStateSenatorsToFile()
 # writeSenContribsToSingleFile()
