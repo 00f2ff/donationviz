b845463db82d7190622175a2d8e7949c173ccd35
Leo Ying <yzy0806@gmail.com>
2015-11-22 13:53:45 -0500

    senator preD3 done

--- public/js/senators.js
+++ public/js/senators.js
@@ -1,7 +1,6 @@
   $(function() {
 
     var data = JSON.parse($('#data-holder').html());
-    console.log(data);
     //portrait
     var namequery= data[0].first_name +"+" +data[0].last_name +"+" +"official" + "+" +"photo";
     var photoQuery="https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q="+namequery+"&imgsz=medium&rsz=1&callback=?";
@@ -22,7 +21,15 @@
     }
     state=statesAbbv[data[0].state];
     var display= party +" senator from " +state;
-    console.log($("#breif").html());
     $("#breif").html(display); 
+ 
 
-  });
+  //Top Contributors
+  for(var i =0;i<5;i++){
+  	var orgName=data[0].records[i].organization;
+  	var total=data[0].records[i].totals;
+  	var individul=data[0].records[i].indivs;
+  	var pac =data[0].records[i].pac;
+  	$("#topContributor").append("<div>"+orgName +" <b>Total $ "+total+"</b>"+"<br>Pac: "+pac+" Individul "+individul + "</div>");
+  }
+});
