9da4f82e572f3ab54858f2e89a306e440c2bc2dc
Duncan McIsaac <dbmcisaac1@gmail.com>
2015-11-23 18:58:09 -0500

    got organizations working fully, and fixed independent bug

--- public/js/organization.js
+++ public/js/organization.js
@@ -13,7 +13,6 @@ $(function() {
       max = data.states[state].total;
     }
   }
-  console.log(min, max);
 
   // right now this styling is done in js; it should move to CSS for non-index files
   $('svg .state').css('fill','#D8D8D8')
@@ -72,6 +71,88 @@ $(function() {
         });
       }
   });
+
+  /* PIE CHARTS 
+   * Broken down by source and party
+   */
+
+  function aggregateData() { // refactored to add some to minmax fxn?
+    var result = {PAC: 0, Individual: 0, Total: 0, Democrat: 0, Republican: 0, Independent: 0}
+    for (var state in data.states) {
+      var stateDonation = data.states[state];
+      // by state
+      result.PAC += stateDonation.pac;
+      result.Individual += stateDonation.individual;
+      result.Total += stateDonation.total;
+      // by party
+      for (var i = 0; i < stateDonation.donations.length; i++) {
+        var senatorDonation = stateDonation.donations[i];
+        var party = senatorDonation.senator.party;
+        if (party === 'R') {
+          result.Republican += senatorDonation.total;
+        } else if (party === 'D') {
+          result.Democrat += senatorDonation.total;
+        } else if (party === 'I') {
+          result.Independent += senatorDonation.total;
+        }
+      }
+    }
+    return result;
+  }
+  var totals = aggregateData();
+
+  function drawPieChart(totals, breakdown) { 
+    var color = {},
+        pieData;
+    if (breakdown === 'source') {
+      color.PAC = "#fde0dd";
+      color.Individual = "#c51b8a";
+      pieData = [{name: "PAC", value: totals.PAC}, {name: "Individual", value: totals.Individual}];
+    } else if (breakdown === 'party') {
+      color.Democrat = 'rgb(33, 150, 243)';
+      color.Republican = 'rgb(244, 67, 54)';
+      color.Independent = '#ffffbf';
+      pieData = [{name: "Democrat", value: totals.Democrat}, {name: "Republican", value: totals.Republican}, {name: "Independent", value: totals.Independent}];
+    }
+
+    var width = 400,
+      height = 400,
+      radius = Math.min(width, height) / 2;
+
+    var arc = d3.svg.arc()
+      .outerRadius(radius - 10)
+      .innerRadius(0);
+
+    var pie = d3.layout.pie()
+      .sort(null)
+      .value(function(d) { return d.value; });
+
+    var svg = d3.select("body").append("svg")
+      .attr("width", width)
+      .attr("height", height)
+    .append("g")
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+
+    var g = svg.selectAll(".arc")
+      .data(pie(pieData))
+    .enter().append("g")
+      .attr("class", "arc");
+
+    g.append("path")
+      .attr("d", arc)
+      .style("fill", function(d) { return color[d.data.name]; });
+
+    g.append("text")
+      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+      .attr("dy", ".35em")
+      .style("text-anchor", "middle")
+      .text(function(d) { if (d.value > 0) return d.value; });
+
+  }
+
+  drawPieChart(totals, 'source');
+  drawPieChart(totals, 'party');
 
 
 
