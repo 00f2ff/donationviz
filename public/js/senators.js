01706443b212f5bbf52396ebfcd623fa3f65fff2
Leo Ying <yzy0806@gmail.com>
2015-12-01 16:22:44 -0500

    graph done

--- public/js/senators.js
+++ public/js/senators.js
@@ -2,8 +2,6 @@
 
     var data = JSON.parse($('#data-holder').html());
     //portrait
-    console.log(data[0]);
-    console.log(data[0].name.replace(/ /,"+"));
     var namequery= data[0].name.replace(/ /,"+") +"+" +"official" + "+" +"photo";
     var photoQuery="https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q="+namequery+"&imgsz=medium&rsz=1&callback=?";
     $.getJSON(photoQuery, function(data){
@@ -34,4 +32,58 @@
   	var pac =data[0].donations[i].pac;
   	$("#topContributor").append("<div>"+orgName +" <b>Total $ "+total+"</b>"+"<br>Pac: "+pac+" Individual "+individual + "</div>");
   }
-});
+
+
+  //Pie Chart
+  var totalIndividual=0;
+  var totalPAC=0
+  for(var i=0;i<100;i++){
+    totalIndividual+=data[0].donations[i].individual;
+    totalPAC+=data[0].donations[i].pac;
+  }
+  var totalAmount=totalPAC+totalIndividual;
+
+  var dataset = [
+  { label: 'Individual', amount: Math.round(totalIndividual/totalAmount*100) }, 
+  { label: 'PAC', amount: Math.round(totalPAC/totalAmount*100) }
+];
+
+  var width = 360;
+  var height = 360;
+  var radius = Math.min(width, height) / 2;     
+  var color = d3.scale.category10();
+
+  var svg = d3.select('#chart')
+  .append('svg')
+  .attr('width', width)
+  .attr('height', height)
+  .append('g')
+  .attr('transform', 'translate(' + (width / 2) +  ',' + (height / 2) + ')');
+
+  var arc = d3.svg.arc()
+  .outerRadius(radius);
+
+  var pie = d3.layout.pie()
+  .value(function(d) { return d.amount; })
+  .sort(null);
+
+  var labelArc = d3.svg.arc()
+    .outerRadius(radius)
+    .innerRadius(radius - 150);
+
+  var g = svg.selectAll(".arc")
+  .data(pie(dataset))
+    .enter().append("g")
+      .attr("class", "arc");
+
+  g.append("path")
+      .attr("d", arc)
+      .style("fill", function(d) { return color(d.data.amount); });
+
+  g.append("text")
+      .attr("transform", function(d) { 
+        return "translate(" + labelArc.centroid(d,i)
+         + ")"; }) 
+      .text(function(d) { return d.data.label +" "+ d.data.amount +"%"; });
+
+  });
